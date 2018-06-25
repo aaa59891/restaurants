53629d69a1354385adda07ae2b815f9baef229fa
Chong <aaa59891@gmail.com>
2018-06-25 11:02:34 +0800

    Updated change the way to set routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,29 +2,14 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import {Request, Response} from "express";
-import {Routes} from "./routes";
-import {User} from "./models/User";
-import { join } from 'path';
-import { OpeningHours } from "./models/OpeningHours";
+import {setRoutes} from "./routes";
 
-createConnection().then(async connection => {
+createConnection().then(async _ => {
     // create express app
     const app = express();
     app.use(bodyParser.json());
-    
     // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method]('/api' + route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result);
-            }
-        });
-    });
+    setRoutes(app);
 
     // setup express app here
     // ...
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,23 +1,37 @@
 import {UserController} from "./controller/UserController";
+import { Application, Request, Response } from 'express';
+import { NextFunction } from "connect";
 
-export const Routes = [{
-    method: "get",
-    route: "/users",
-    controller: UserController,
-    action: "all"
-}, {
-    method: "get",
-    route: "/users/:id",
-    controller: UserController,
-    action: "one"
-}, {
-    method: "post",
-    route: "/users",
-    controller: UserController,
-    action: "save"
-}, {
-    method: "delete",
-    route: "/users",
-    controller: UserController,
-    action: "remove"
-}];
+export function setRoutes(app: Application){
+    const userController = new UserController();
+    const routes = [{
+        method: "get",
+        route: "/users",
+        controller: userController.all.bind(userController)
+    }, {
+        method: "get",
+        route: "/users/:id",
+        controller: userController.one.bind(userController)
+    }, {
+        method: "post",
+        route: "/users",
+        controller: userController.save.bind(userController)
+    }, {
+        method: "delete",
+        route: "/users",
+        controller: userController.remove.bind(userController)
+    }];
+
+
+    for(let route of routes){
+        app[route.method]('/api' + route.route, (req: Request, res: Response, next: NextFunction) => {
+            const result = route.controller(req, res, next);
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+
+            } else if (result !== null && result !== undefined) {
+                res.json(result);
+            }
+        });
+    }
+}
