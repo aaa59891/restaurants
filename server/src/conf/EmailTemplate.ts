3c491d97c7ad652eba2381dce4c240a5db39171b
Chong <aaa59891@gmail.com>
2018-06-28 19:48:13 +0800

    Fixed start.sh and email content template

--- server/src/conf/EmailTemplate.ts
+++ server/src/conf/EmailTemplate.ts
@@ -6,7 +6,6 @@ export const CollaborationEmail: Email = {
     subject: 'Collaborate on favorite restaurants from your friend!',
     text: `
     Here is the url: {url} .
-    
     You can collaborate on the favorite restaurants with your friend!
     `
 }
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 cd client
 echo "Building angular..."
 ng build --prod
-cp dist ../server/src
+cp -r dist ../server/src
 cd ../server
 cd server
 NODE_ENV=prod npm start
